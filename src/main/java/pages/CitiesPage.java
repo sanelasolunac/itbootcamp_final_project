0b09db18957476197de5d438d9fde788a00d9190
Sanela Solunac <sanelasolunac995@gmail.com>
2022-06-20 19:11:33 +0200

    Cities Page

---
+++ src/main/java/pages/CitiesPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CitiesPage {
+    private WebDriver driver;
+
+    public CitiesPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
