d35db362642803fd68cd8b7604099aea3241af97
Sanela Solunac <sanelasolunac995@gmail.com>
2022-06-20 19:08:30 +0200

    Navigation Page

---
+++ src/main/java/pages/NavPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class NavPage {
+    private WebDriver driver;
+
+    public NavPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
