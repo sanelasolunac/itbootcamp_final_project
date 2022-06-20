6a530f1c3dae76e67941c1023795ee007e0d799a
Sanela Solunac <sanelasolunac995@gmail.com>
2022-06-20 19:12:38 +0200

    Message PopUp Page

---
+++ src/main/java/pages/MessagePopUpPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class MessagePopUpPage {
+    private WebDriver driver;
+
+    public MessagePopUpPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
