70af4f54be34b8fdd4602b4947cc2c422d31492a
Sanela Solunac <sanelasolunac995@gmail.com>
2022-06-20 19:09:50 +0200

    SignUp Page

---
+++ src/main/java/pages/SignupPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class SignupPage {
+    private WebDriver driver;
+
+    public SignupPage(WebDriver driver) {
+        this.driver = driver;
+    }
+}
