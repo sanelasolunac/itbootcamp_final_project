8b3e79fd40a32771a9e16c7303e6a1cdd3883f2d
Sanela Solunac <sanelasolunac995@gmail.com>
2022-06-21 21:21:45 +0200

    Login Test update

--- src/main/java/tests/LoginTests.java
+++ src/main/java/tests/LoginTests.java
@@ -1,7 +1,14 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class LoginTests extends BasicTest{
 
@@ -11,7 +18,7 @@ public class LoginTests extends BasicTest{
 //    Klik na login dugme iz navigacije
 //    Verifikovati da se u url-u stranice javlja ruta /login
 
-    @Test
+    @Test (priority = 1)
     public void visitLoginPage(){
         this.driver.get(baseUrl);
         navPage.getLanguageButton().click();
@@ -20,6 +27,113 @@ public class LoginTests extends BasicTest{
 
         Assert.assertEquals(this.driver.getCurrentUrl(),
                 baseUrl + "/login",
-                "[ERROR] The URL is not valid.");
+                "[ERROR] The URL does not contain /login part.");
     }
+
+    @Test (priority = 2)
+    public void checkInputTypes (){
+        navPage.getLoginButton().click();
+
+        Assert.assertEquals(loginPage.getEmailInput().getAttribute("type"),
+                "email",
+                "[ERROR] Attributes does not contain type 'email'.");
+
+        Assert.assertEquals(loginPage.getPasswordInput().getAttribute("type"),
+                "password",
+                "[ERROR] Attributes does not contain type 'password'.");
+    }
+    @Test(priority = 3)
+    public void displayErrorsWhenUserDoesNotExist (){
+//        email: non-existing-user@gmal.com
+//        password: password123
+//        Koraci:
+//        Klik na login dugme iz navigacije
+//        Popuniti login formu podacima za logovanje
+//        Klik na login dugme
+//        Sacekati da popu za prikaz greske bude vidljiv
+//        Verifikovati da greska sadrzi poruku User does not exists
+//        Verifikovati da se u url-u stranice javlja /login ruta
+
+        navPage.getLoginButton().click();
+
+        loginPage.getEmailInput().sendKeys("non-existing-user@gmal.com");
+        loginPage.getPasswordInput().sendKeys("password123");
+        loginPage.getSubmitLoginButton().click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@role, 'status')]")));
+
+        Assert.assertEquals(this.driver.findElement(By.xpath("//div[contains(@role, 'status')]/ul/li")).getText(),
+                "User does not exists",
+                "[ERROR] Text does not contain 'User does not exists'.");
+
+        Assert.assertEquals(this.driver.getCurrentUrl(),
+                baseUrl + "/login",
+                "[ERROR] This URL does not have /login.");
+    }
+    @Test(priority = 4)
+    public void displayErrorsWhenPasswordIsWrong (){
+//        Podaci:
+//        email: admin@admin.com
+//        password: password123
+//        Koraci:
+//        Klik na login dugme iz navigacije
+//        Popuniti login formu podacima za logovanje
+//        Klik na login dugme
+//        Sacekati da popu za prikaz poruke bude vidljiv
+//        Verifikovati da greska sadrzi poruku Wrong password
+//        Verifikovati da se u url-u stranice javlja /login ruta
+
+        navPage.getLoginButton().click();
+
+        loginPage.getEmailInput().sendKeys("admin@admin.com");
+        loginPage.getPasswordInput().sendKeys("password123");
+        loginPage.getSubmitLoginButton().click();
+
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'error')]")));
+
+        Assert.assertEquals(this.driver.findElement(By.xpath("//div[contains(@role, 'status')]/ul/li")).getText(),
+                "Wrong password",
+                "[ERROR] Text does not contain 'Wrong Password'.");
+
+        Assert.assertEquals(this.driver.getCurrentUrl(),
+                baseUrl + "/login",
+                "[ERROR] This URL does not have /login.");
+    }
+    @Test (priority = 5)
+    public void logIn (){
+//        Podaci:
+//        email: admin@admin.com
+//        password: 12345
+//        Koraci:
+//        Klik na login dugme iz navigacije
+//        Popuniti login formu podacima za logovanje
+//        Verifikovati da se u url-u stranice javlja /home ruta
+
+        navPage.getLoginButton().click();
+
+        loginPage.getEmailInput().sendKeys("admin@admin.com");
+        loginPage.getPasswordInput().sendKeys("12345");
+        loginPage.getSubmitLoginButton().click();
+
+        Assert.assertEquals(this.driver.getCurrentUrl(),
+                baseUrl + "/home",
+                "[ERROR] This URL does not have /home.");
+    }
+    @Test (priority = 6)
+    public void logOut (){
+//        Koraci:
+//        Verifikovati da je dugme logout vidljivo na stranici
+//        Kliknuti na logout dugme
+
+
+
+        Assert.assertTrue(navPage.getLogOutButton().isDisplayed(),
+                "[ERROR] The element does not exists.");
+
+        navPage.getLoginButton().click();
+
+    }
+
 }
