613a76c5de2bfcebe874487b3e6c25fa96fa0e1c
Sanela Solunac <sanelasolunac995@gmail.com>
2022-06-22 17:53:13 +0200

    SignUp Page finished

--- src/main/java/pages/SignupPage.java
+++ src/main/java/pages/SignupPage.java
@@ -1,11 +1,28 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class SignupPage {
     private WebDriver driver;
 
     public SignupPage(WebDriver driver) {
         this.driver = driver;
+    }
+    public WebElement getInputName (){
+        return this.driver.findElement(By.id("name"));
+    }
+    public WebElement getInputEmail(){
+        return this.driver.findElement(By.id("email"));
+    }
+    public WebElement getInputPassword(){
+        return this.driver.findElement(By.id("password"));
+    }
+    public WebElement getConfirmPassword(){
+        return this.driver.findElement(By.id("confirmPassword"));
+    }
+    public WebElement getSignMeUpButton(){
+        return this.driver.findElement(By.xpath("//button[contains(@type, 'submit')]"));
     }
 }
