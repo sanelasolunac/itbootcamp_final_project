2f7a2c0c52b077e225a04a9e18a84f54be1ae2d9
Sanela Solunac <sanelasolunac995@gmail.com>
2022-06-23 00:49:09 +0200

    Cities Page updated

--- src/main/java/pages/CitiesPage.java
+++ src/main/java/pages/CitiesPage.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
 
 public class CitiesPage {
     private WebDriver driver;
@@ -23,7 +24,10 @@ public class CitiesPage {
     }
     public void waitForNewEditItemBoxToBeVisible(){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'dlgNewEditItem')]")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'v-dialog dlgNewEditItem')]")));
+    }
+    public WebElement getNewItemInput(){
+        return this.driver.findElement(By.xpath("// input [@id= 'name']"));
     }
 //    metodu koja ceka da se dijalog za editovanje i kreiranje pojavi
 //    VALJDA JE TO SAMO OVA JEDNA A NE DVE
@@ -41,8 +45,30 @@ public class CitiesPage {
         return this.driver.findElement(By.xpath("//button[contains(@class, 'btnSave')]"));
     }
     public WebElement getDeleteButton(){
-        return this.driver.findElement(By)
+        return this.driver.findElement(By.xpath("//button/span[contains(text(),'Delete')]"));
+    }
+    public void waitForSuccessfullyPopUpToBeVisible(){
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(text(),' Saved successfully ')]")));
+    }
+//    metodu koja ceka da se u tabeli pojavi odredjeni broj redova. Metoda kao parametar prima broj redova.
+//    Ako se metoda pozove sa parametrom 5, to znaci da metoda ceka da se u tabeli pojavi 5 redova.
+
+    public void waitForNumbersOfRowsToBe (int rowIndex) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.tagName("tr"), rowIndex +1 ));
     }
 
 
+    public WebElement getEditButtonFromRow (int row){
+        return driver.findElement(By.xpath("//tbody/tr[" + row + "]/td/div/button[@id = 'edit']"));
+    }
+
+    public WebElement getDeleteButtonFromRow (int row){
+        return driver.findElement(By.xpath("//tbody/tr[" + row + "]/td/div/button[@id = 'delete']"));
+    }
+    public WebElement getTableCell(int rowIndex, int columnIndex) {
+        return driver.findElement(By.xpath(
+                        "//tbody/tr[" + rowIndex + "]/td[" + columnIndex + "]"));
+    }
 }
