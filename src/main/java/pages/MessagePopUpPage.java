f7c9f26b8e749921dae7b166937081238b629ccd
Sanela Solunac <sanelasolunac995@gmail.com>
2022-06-25 16:14:35 +0200

    Message Pop Up Page finished

--- src/main/java/pages/MessagePopUpPage.java
+++ src/main/java/pages/MessagePopUpPage.java
@@ -35,7 +35,6 @@ public class MessagePopUpPage {
                 .xpath("//div[contains(@class, 'v-snack')]/div[contains(@class, 'success')]")));
 
     }
-
     public WebElement getElementsWithVisibleMessagesForCities(){
         return driver.findElement(By.xpath("//div[contains(@class, 'v-snack')]/div[contains(@class, 'success')]"));
     }
